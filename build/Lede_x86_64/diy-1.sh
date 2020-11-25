2cb94a47b302be03beebcf58bb2470f72e3671e2
XBaiLong <2995773594@qq.com>
2020-11-25 19:25:20 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -4,9 +4,6 @@
 
 # 使用源码自带ShadowSocksR Plus+出国软件
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
-# 使用源码自带ShadowSocksR Plus+出国软件
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy  #主题-rosy
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge  #主题-edge
